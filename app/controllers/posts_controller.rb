6411cbc124afca439bb1bb000891c3ecdac945af
kunihito-uehara <82615825+kunihito-uehara@users.noreply.github.com>
2021-07-14 20:39:18 +0900

    ついーと課題

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+   before_action :set_post, only: [:show, :edit, :update,:destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    elsif @post.save
+      redirect_to posts_path,notice: "ブログを作成"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"削除しました！！"
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
